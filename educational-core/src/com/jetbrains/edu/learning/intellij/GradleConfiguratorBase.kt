bfc440a2304ee7f79d65fb0a79968dc8c1553926
Liana.Bakradze <liana.bakradze@jetbrains.com>
2018-07-19 13:42:32 +0300

    EDU-1551 Add changed settings.gradle file to course

--- educational-core/src/com/jetbrains/edu/learning/intellij/GradleConfiguratorBase.kt
+++ educational-core/src/com/jetbrains/edu/learning/intellij/GradleConfiguratorBase.kt
@@ -1,16 +1,25 @@
 package com.jetbrains.edu.learning.intellij
 
+import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.util.containers.ContainerUtil
 import com.jetbrains.edu.learning.EduConfigurator
 import com.jetbrains.edu.learning.EduNames
+import org.jetbrains.plugins.gradle.util.GradleConstants
+import java.io.File
 
 abstract class GradleConfiguratorBase : EduConfigurator<JdkProjectSettings> {
 
   override fun excludeFromArchive(path: String): Boolean {
     val pathSegments = FileUtil.splitPath(path)
     val name = pathSegments.last()
+    if (GradleConstants.SETTINGS_FILE_NAME == name) {
+      val courseBuilder = courseBuilder as GradleCourseBuilderBase
+      val templateText = FileTemplateManager.getDefaultInstance().getInternalTemplate(courseBuilder.settingGradleTemplateName).getText(
+        courseBuilder.configMap)
+      return FileUtil.loadFile(File(path)) == templateText
+    }
     return name in NAMES_TO_EXCLUDE || pathSegments.any { it in FOLDERS_TO_EXCLUDE } || "iml" == FileUtilRt.getExtension(name)
   }
 
@@ -22,7 +31,7 @@ abstract class GradleConfiguratorBase : EduConfigurator<JdkProjectSettings> {
   companion object {
     private val NAMES_TO_EXCLUDE = ContainerUtil.newHashSet(
       ".idea", "EduTestRunner.java", "gradlew", "gradlew.bat", "local.properties", "gradle.properties",
-      "settings.gradle", "gradle-wrapper.jar", "gradle-wrapper.properties")
+      GradleConstants.SETTINGS_FILE_NAME, "gradle-wrapper.jar", "gradle-wrapper.properties")
 
     private val FOLDERS_TO_EXCLUDE = ContainerUtil.newHashSet(EduNames.OUT, EduNames.BUILD)
   }
